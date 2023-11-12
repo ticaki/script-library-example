33960ae6206333ba122c4370a9fc953707a88cb0
ticaki <tim.kiemen@renopoint.de>
2023-11-12 18:14:01 +0100

    rename that in adapter

--- lib.js
+++ lib.js
@@ -1,15 +1,15 @@
 class Person {
-    constructor(vorname, nachname, alter, that) {
+    constructor(vorname, nachname, alter, adapter) {
         this.vorname = vorname;
         this.nachname = nachname;
         this.alter = alter;
-        this.that = that;
+        this.adapter = adapter;
     }
     information() {
         return (`Mein Name ist ${this.vorname} ${this.nachname} und ich bin ${this.alter} Jahre alt!!!!1`);
     }
     log() {
-        this.that.log(this.information());
+        this.adapter.log(this.information());
     }
 }
 module.exports = { Person };
--- lib.ts
+++ lib.ts
@@ -2,19 +2,19 @@ class Person {
     private nachname: string;
     private vorname: string;
     private alter: string;
-    private that: any;
-    constructor(vorname, nachname, alter, that) {
+    private adapter: any;
+    constructor(vorname, nachname, alter, adapter) {
       this.vorname = vorname;
       this.nachname = nachname;
       this.alter = alter;
-      this.that = that;
+      this.adapter = adapter;
     }
   
     information(): string {
       return (`Mein Name ist ${this.vorname} ${this.nachname} und ich bin ${this.alter} Jahre alt!!!!1`);
     }
     log(): void {
-        this.that.log(this.information());
+        this.adapter.log(this.information());
     }
   }
  
